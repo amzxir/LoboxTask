9a3aa661b0c2c2f0c24d9e7fd041726351a11f69
amzxir <amirreza79ahmadi@gmail.com>
2025-05-19 10:54:12 +0330

    config vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, type AliasOptions } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+    } as AliasOptions,
+  },
   plugins: [react()],
-})
+});
